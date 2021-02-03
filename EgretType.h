c3b784a9ad303c561764d6827e1f76fcdf157fb4
TinyWang <2746975606@qq.com>
2021-02-03 18:59:20 +0800

    Add a type.

--- EgretType.h
+++ EgretType.h
@@ -1,6 +1,7 @@
 #ifndef EgretType_Header
 #define EgretType_Header
 
+#include "EgretFont.h"
 #include "EgretHeader.h"
 #include _EgretSDL2_stdinc
 
@@ -10,6 +11,7 @@ extern "C" {
 
 typedef SDL_bool EgretBool;
 
+typedef Uint8 EgretInt8;
 typedef Uint32 EgretInt32;
 
 #ifdef __cplusplus
